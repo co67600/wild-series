3f311bdb21de67ba6b8c010870807024a6209ae0
corinne <cdallongeville22@gmail.com>
2020-04-06 18:47:01 +0200

    requetes personnalisees

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,6 +1,9 @@
 <?php
 namespace App\DataFixtures;
 
+use App\Entity\Category;
+use App\Entity\Program;
+use App\Entity\Actor;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -8,8 +11,28 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = \Faker\Factory::create('fr_FR');
+
+        for ($i = 1; $i <= 1000; $i++) {
+            $category = new Category();
+            $category->setName($faker->word);
+            $manager->persist($category);
+            $this->addReference("category_".$i, $category);
+
+            $program = new Program();
+            $program->setTitle($faker->sentence(4, true));
+            $program->setSummary($faker->text(100));
+            $program->setCategory($this->getReference("category_".rand(0,1000)));
+            $this->addReference("program_".$i, $program);
+            $manager->persist($program);
+
+            for($j = 1; $j <= 5; $j ++) {
+                $actor = new Actor();
+                $actor->setName($faker->name);
+                $actor->addProgram($this->getReference(“program_”.$i));
+                $manager->persist($actor);
+            }
+        }
         $manager->flush();
     }
 }
